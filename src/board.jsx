52c2c034994c3ff50de5d6cefefaa41a9ce27dc3
harshitpd910 <harshitprasad1@gmail.com>
2023-10-26 16:41:54 +0530

    Adding complete file for the first time

---
+++ src/board.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useEffect } from 'react';
+import './board.css'
+
+const Board = () => {
+
+  const clickHandler = (element) => {
+    const targetDiv = element.target.className.split(" ").filter((classname) => classname != "indiv")[0];
+    console.log(`target div is : ${targetDiv}`)
+  }
+
+  useEffect(() => {
+
+    document.querySelectorAll('.indiv').forEach((comp)=>{comp.addEventListener('keyup',clickHandler)})
+
+
+  }
+  ,[])
+
+  window.addEventListener('keyup',clickHandler)
+
+  // let a =new Array(9);
+  let a = Array.from({length:9})   
+   
+
+  return (
+    <>
+    <div className="main">
+      <div className="header">
+        <h1>TIC-TAC-TOE</h1>
+      </div>
+      <div className="body">
+        <div className="leftbar">
+          {/* <h1>body</h1> */}
+        </div>
+        <div className="boardbox">
+          <div className="boardgame">
+            {/* { Array.from({ length:9}).map((item,index)=>{return <div className=  {`indiv indiv${index+1} `} >as</div>})} */}
+            {
+              a.map((_,index)=> <div className={`indiv indiv${index+1}`}>25</div>)
+            }          
+          </div>
+          {/* {
+            document.querySelectorAll(".indiv").forEach((element) => 
+              element.addEventListener("click",clickHandler)
+            )
+          } */}
+        </div>
+        <div className="rightbar">
+
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
+     
+    </div>
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Board
